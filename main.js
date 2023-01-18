ae54e44edd04feb41162f22ac0c69b07ab3378c9
JFerrettUK <jferrettuk@gmail.com>
2023-01-18 09:02:58 +0000

    Details added to relevant pages.

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 function loginWarning() {
   alert('Login and logout features are in development.');
 }
+
+function video() {
+  alert('Video download features are in development.');
+}
+
+window.onload = function () {
+  function what() {
+    document.getElementById('header_1').innerHTML = 'hi';
+  }
+  what();
+};
