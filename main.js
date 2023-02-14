730c2995267d7836c55ec2fec9ada00ebe9202d7
JFerrettUK <jferrettuk@gmail.com>
2023-02-14 16:50:56 +0000

    CSS and HTML updates to website

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
 /* eslint-disable no-alert */
 /* eslint-disable no-unused-vars */
 function loginWarning() {
@@ -6,4 +8,15 @@ function loginWarning() {
 
 function video() {
   alert('Video download features are in development.');
+}
+
+function toggle(n) {
+  const menus = document.getElementsByClassName('submenu');
+  for (let i = 0; i < menus.length; i++) {
+    if ((i == (n - 1)) && (menus[i].style.display != 'block')) {
+      menus[i].style.display = 'block';
+    } else {
+      menus[i].style.display = 'none';
+    }
+  }
 }
